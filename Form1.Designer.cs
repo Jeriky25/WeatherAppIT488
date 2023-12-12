66bbbbd579fc4ed2fbe076b981490e9434ef9242
Jeriel Diaz <balayan25@gmail.com>
2023-12-11 18:49:44 -0600

    Added min max temp, wind speed, and humidity.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,15 +31,20 @@
             this.label1 = new System.Windows.Forms.Label();
             this.txtCity = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.txtWeather = new System.Windows.Forms.TextBox();
+            this.txtMaxTemp = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.txtMinTemp = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.txtWindSpeed = new System.Windows.Forms.TextBox();
+            this.btnShowWeather = new System.Windows.Forms.Button();
+            this.txtHumidity = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(97, 96);
+            this.label1.Location = new System.Drawing.Point(67, 58);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(24, 13);
             this.label1.TabIndex = 0;
@@ -47,7 +52,7 @@
             // 
             // txtCity
             // 
-            this.txtCity.Location = new System.Drawing.Point(200, 93);
+            this.txtCity.Location = new System.Drawing.Point(177, 55);
             this.txtCity.Name = "txtCity";
             this.txtCity.Size = new System.Drawing.Size(100, 20);
             this.txtCity.TabIndex = 1;
@@ -55,43 +60,90 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(97, 158);
+            this.label2.Location = new System.Drawing.Point(67, 99);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.Size = new System.Drawing.Size(73, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Weather";
+            this.label2.Text = "Max Temp. °F";
             // 
-            // txtWeather
+            // txtMaxTemp
             // 
-            this.txtWeather.Location = new System.Drawing.Point(200, 155);
-            this.txtWeather.Name = "txtWeather";
-            this.txtWeather.Size = new System.Drawing.Size(100, 20);
-            this.txtWeather.TabIndex = 3;
+            this.txtMaxTemp.Location = new System.Drawing.Point(177, 96);
+            this.txtMaxTemp.Name = "txtMaxTemp";
+            this.txtMaxTemp.Size = new System.Drawing.Size(100, 20);
+            this.txtMaxTemp.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(97, 211);
+            this.label3.Location = new System.Drawing.Point(67, 143);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.Size = new System.Drawing.Size(70, 13);
             this.label3.TabIndex = 4;
-            this.label3.Text = "Wind Speed km/h";
+            this.label3.Text = "Min Temp. °F";
+            // 
+            // txtMinTemp
+            // 
+            this.txtMinTemp.Location = new System.Drawing.Point(177, 140);
+            this.txtMinTemp.Name = "txtMinTemp";
+            this.txtMinTemp.Size = new System.Drawing.Size(100, 20);
+            this.txtMinTemp.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(67, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Wind Speed mph";
             // 
             // txtWindSpeed
             // 
-            this.txtWindSpeed.Location = new System.Drawing.Point(200, 208);
+            this.txtWindSpeed.Location = new System.Drawing.Point(177, 181);
             this.txtWindSpeed.Name = "txtWindSpeed";
             this.txtWindSpeed.Size = new System.Drawing.Size(100, 20);
-            this.txtWindSpeed.TabIndex = 5;
+            this.txtWindSpeed.TabIndex = 7;
+            // 
+            // btnShowWeather
+            // 
+            this.btnShowWeather.Location = new System.Drawing.Point(571, 283);
+            this.btnShowWeather.Name = "btnShowWeather";
+            this.btnShowWeather.Size = new System.Drawing.Size(96, 38);
+            this.btnShowWeather.TabIndex = 8;
+            this.btnShowWeather.Text = "Show Weather";
+            this.btnShowWeather.UseVisualStyleBackColor = true;
+            this.btnShowWeather.Click += new System.EventHandler(this.btnShowWeather_Click);
+            // 
+            // txtHumidity
+            // 
+            this.txtHumidity.Location = new System.Drawing.Point(177, 226);
+            this.txtHumidity.Name = "txtHumidity";
+            this.txtHumidity.Size = new System.Drawing.Size(100, 20);
+            this.txtHumidity.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(67, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Humidity";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtHumidity);
+            this.Controls.Add(this.btnShowWeather);
             this.Controls.Add(this.txtWindSpeed);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMinTemp);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtWeather);
+            this.Controls.Add(this.txtMaxTemp);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtCity);
             this.Controls.Add(this.label1);
@@ -108,9 +160,14 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtCity;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtWeather;
+        private System.Windows.Forms.TextBox txtMaxTemp;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMinTemp;
+        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtWindSpeed;
+        private System.Windows.Forms.Button btnShowWeather;
+        private System.Windows.Forms.TextBox txtHumidity;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,42 +10,45 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using RestSharp;
+using System.Xml.Linq;
 
 namespace WeatherApp
 {
     public partial class Form1 : Form
     {
+        string city;
+        string maxTemp;
+        string minTemp;
+        string windSpeed;
+        string humidity;
+        
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void GetWeatherData(string location)
-        {
-            var client = new RestClient($"https://wttr.in/{WebUtility.UrlEncode(location)}?format=%C+%t+%w+%h");
 
-            var request = new RestRequest();
-            request.AddParameter("method", "GET");
-
-            var response = client.Execute(request);
-
-            if (response.IsSuccessful)
-            {
-                string[] weatherParameters = Regex.Split(response.Content, " ");
-
-                Console.WriteLine("Weather: " + weatherParameters[0]);
-                Console.WriteLine("Tempurature: " + weatherParameters[1]);
-                Console.WriteLine("Wind: " + weatherParameters[2]);
-            }
-            else
-            {
-                MessageBox.Show("Error: " + response.StatusCode);
-            }
-        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            GetWeatherData("Carol Stream");
+            
+        }
+
+        private void btnShowWeather_Click(object sender, EventArgs e)
+        {
+            city = txtCity.Text;
+            string url = string.Format("http://api.weatherapi.com/v1/forecast.xml?key=46d22bedb4824b5485a172113201209&q={0}&days=1", city);
+            XDocument document = XDocument.Load(url);
+
+            maxTemp = (string)document.Descendants("maxtemp_f").FirstOrDefault();
+            minTemp = (string)document.Descendants("mintemp_f").FirstOrDefault();
+            windSpeed = (string)document.Descendants("maxwind_mph").FirstOrDefault();
+            humidity = (string)document.Descendants("avghumidity").FirstOrDefault();
+
+            txtMaxTemp.Text = maxTemp;
+            txtMinTemp.Text = minTemp;
+            txtWindSpeed.Text = windSpeed;
+            txtHumidity.Text = humidity;
         }
     }
 }
